acdf3e05c97d8d98bc349847fa76ec33fd033569
MJ Harvey <m.j.harvey@acellera.com>
2017-11-06 13:01:16 +0000

    first working version

--- src/recharge/recharge.py
+++ src/recharge/recharge.py
@@ -3,22 +3,29 @@ import os
 import copy
 
 class GL:
-	def __init__( self, company="IC", cost_centre="ITSO", activity="G80447", account="163116" ):
+	def __init__( self, company="IC", cost_centre="ITSO", activity="G80410", analysis="165128", subanalysis1 = "AHO", subanalysis2 = "0", description = "Research Computing Service Recharge" ):
 		if not isinstance (company, str) :
 			raise Exception("company is not a string")
 		if not isinstance( cost_centre, str) :
 			raise Exception("cost_centre is not a string")
 		if not isinstance( activity, str) :
 			raise Exception("activity is not a string")
-		if not isinstance(account, str) :
-			raise Exception("account is not a string")
+		if not isinstance(analysis, str) :
+			raise Exception("analysis is not a string")
+		if not isinstance(subanalysis1, str) :
+			raise Exception("subanalysis1 is not a string")
+		if not isinstance(subanalysis2, str) :
+			raise Exception("subanalysis2 is not a string")
 		self.company     = company
 		self.cost_centre = cost_centre
 		self.activity    = activity
-		self.account     = account
+		self.analysis     = int(analysis)
+		self.subanalysis1 = subanalysis1
+		self.subanalysis2 = subanalysis2
+		self.description  = description
 	
 	def __str__(self):
-		return "%s-%s-%s-%s" % ( self.company, self.cost_centre, self.activity, self.account )
+		return "%s.%s.%s.%s.%s.%s" % ( self.company, self.cost_centre, self.activity, self.analysis, self.subanalysis1, self.subanalysis2 )
 
 class Recharge:
 	def __init__(self, description=None, destination=None):
@@ -70,6 +77,8 @@ class Recharge:
 
 		sheet[ "J%d" % (23 + len(self.transactions)*2) ].value = "=SUM(J23:J%d)" % ( 24 + len(self.transactions)*2 - 2)
 		sheet[ "K%d" % (23 + len(self.transactions)*2) ].value = "=SUM(K23:K%d)" % ( 24 + len(self.transactions)*2 - 2)
+		sheet[ "E12" ] = "=TODAY()"
+		sheet[ "E14" ] = sheet[ "E16" ] = self.description
 
 		for row in range( 1, len(self.transactions )):
 			for i in [23, 24]:
@@ -91,26 +100,26 @@ class Recharge:
 			sheet[ "C%d" % ( idx ) ] = t.company
 			sheet[ "D%d" % ( idx ) ] = t.cost_centre
 			sheet[ "E%d" % ( idx ) ] = t.activity
-			sheet[ "F%d" % ( idx ) ] = t.account
+			sheet[ "F%d" % ( idx ) ] = t.analysis
 			sheet[ "G%d" % ( idx ) ] = "0"
-			sheet[ "H%d" % ( idx ) ] = "0"
-			sheet[ "I%d" % ( idx ) ] = "0"
+			sheet[ "H%d" % ( idx ) ] = self.destination.subanalysis1
+			sheet[ "I%d" % ( idx ) ] = self.destination.subanalysis2
 			sheet[ "J%d" % ( idx ) ] = t.amount
 			sheet[ "K%d" % ( idx ) ] = ""
-			sheet[ "L%d" % ( idx ) ] = t.description
+			sheet[ "L%d" % ( idx ) ] = t.description + "[ rcs-manager@imperial.ac.uk ]"
 			idx = idx + 1
 
 #			sheet[ "B%d" % ( idx ) ] = "C" 
-			sheet[ "C%d" % ( idx ) ] = t.company
-			sheet[ "D%d" % ( idx ) ] = t.cost_centre
-			sheet[ "E%d" % ( idx ) ] = t.activity
-			sheet[ "F%d" % ( idx ) ] = t.account
+			sheet[ "C%d" % ( idx ) ] = self.destination.company
+			sheet[ "D%d" % ( idx ) ] = self.destination.cost_centre
+			sheet[ "E%d" % ( idx ) ] = self.destination.activity
+			sheet[ "F%d" % ( idx ) ] = self.destination.analysis
 			sheet[ "G%d" % ( idx ) ] = "R"
-			sheet[ "H%d" % ( idx ) ] = "0"
-			sheet[ "I%d" % ( idx ) ] = "0"
+			sheet[ "H%d" % ( idx ) ] = self.destination.subanalysis1
+			sheet[ "I%d" % ( idx ) ] = self.destination.subanalysis2
 			sheet[ "J%d" % ( idx ) ] = ""
 			sheet[ "K%d" % ( idx ) ] = t.amount
-			sheet[ "L%d" % ( idx ) ] = "Recharge for " + t.description + "(" + str(t) + ")"
+			sheet[ "L%d" % ( idx ) ] = "Recharge for " + t.description + "(" + str(t) + ") "
 			idx = idx + 1
 
 		wb.save( filename = filename )
@@ -119,10 +128,10 @@ class Recharge:
 
 if __name__ == "__main__":
 	print("Hello")	
-	r = Recharge( description="X" , destination = GL() )
-	src = GL( cost_centre="EDTA", activity = "G80000", account = "123456" )	
-	r.addTransaction( src, 100.0, "Recharge amount" )
+	r = Recharge( description="Recharge for CX2 compute time" , destination = GL( cost_centre="ITSO", activity ="G80410", analysis="165128" ) )
+#	src = GL( cost_centre="MEDY", activity = "F46000" )
+#	r.addTransaction( src, 21340.00, "R29598  1152 cores (72 16c nodes) for 60 days @ 355.70/day" )
+#	r.addTransaction( src, 5954.17,  "R44565  624 cores (26 24c nodes) for 31 days @ 192.07/day: 5,954.17" )
+#	r.addTransaction( src, 2688.98,  "R51372  624 cores (26 24c nodes) for 14 days @ 192.07/day: 2,688.98" )
 
-	src = GL( cost_centre="XDTA", activity = "G81235", account = "987654" )	
-	r.addTransaction( src, 500.0, "Recharge amount for compute" )
-	r.writeOutput( "/tmp/xx.xlsm" )
+	r.writeOutput( "/tmp/mehdi-recharge.xlsm" )
